a34aff519734edeb582fe5e6c406fbf105daa984
ManhHung4120 <94128344+ManhHung4120@users.noreply.github.com>
2021-11-25 22:16:57 +0700

    Add files via upload

--- src/java/dao/DAO.java
+++ src/java/dao/DAO.java
@@ -1,28 +1,20 @@
-
-package DAO;
+package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-
-/**
- *
- * @author hungt
- */
+ 
 public class DAO {
-    private static final String DB_URL = "jdbc:mysql://localhost:3307/dat_do_an?allowPublicKeyRetrieval=true&autoReconnect=true&useSSL=false";
-    private static final String USER_NAME = "root";
-    private static final String PASSWORD = "root";
     public static Connection con;
-    
-   
-    public DAO() {
-        if (con == null) {
-            String dbClass = "com.mysql.cj.jdbc.Driver";
+     
+    public DAO(){
+        if(con == null){
+            String dbUrl = "jdbc:mysql://localhost:3307/hotel?autoReconnect=true&useSSL=false";
+            String dbClass = "com.mysql.jdbc.Driver";
+ 
             try {
                 Class.forName(dbClass);
-                con = DriverManager.getConnection (DB_URL, USER_NAME, PASSWORD);
-                System.out.println("connect successfully!");
-            } catch (Exception e) {
+                con = DriverManager.getConnection (dbUrl, "root", "xxx");
+            }catch(Exception e) {
                 e.printStackTrace();
             }
         }
--- src/java/dao/HoaDonDAO.java
+++ src/java/dao/HoaDonDAO.java
@@ -5,10 +5,38 @@
  */
 package dao;
 
+import static dao.DAO.con;
+import java.sql.PreparedStatement;
+import model.HoaDon;
+
 /**
  *
- * @author hungt
+ * @author Hung
  */
-public class HoaDonDAO {
+public class HoaDonDAO extends DAO{
     
-}
+    public HoaDonDAO(){
+        super();
+    }
+    
+    public boolean luuHoaDon(HoaDon hoadon){
+          boolean result = false;
+         java.util.Date now = new java.util.Date();
+        java.sql.Date date = new java.sql.Date(now.getTime());
+        String sql = "INSERT INTO tblHoaDon(thoigianxuat,tienthanhtoan,tblDonHangId) VALUES (?,?,?) ";
+        try{
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setDate(1, date);
+            ps.setFloat(2, hoadon.getTienthanhtoan());
+            ps.setInt(3, hoadon.getDonhang().getId());
+             result = ps.execute();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+        
+        
+    }
+    
+
